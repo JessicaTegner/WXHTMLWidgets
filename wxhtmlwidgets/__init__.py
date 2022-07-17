090667caa03e3ae863178ed3db93954d85972818
NicklasTegner <NicklasMCHD@live.dk>
2022-07-17 10:38:03 +0200

    panel

--- wxhtmlwidgets/__init__.py
+++ wxhtmlwidgets/__init__.py
@@ -2,6 +2,44 @@ from re import I
 import wx
 import wx.html2
 
+class AccessibleHTMLPanel(wx.Panel):
+    def __init__(self, *args, **kwargs):
+        wx.panel.__init__(self, *args, **kwargs)
+        self.html = wx.html2.WebView.New(self)
+        self.buttonSizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.focuser = wx.UIActionSimulator()
+        wx.CallAfter(self.focuser.MouseMove, self.html.GetScreenPosition())
+        wx.CallAfter(self.focuser.MouseClick) 
+        self.html.Bind(wx.EVT_SET_FOCUS, self.OnSetFocus)
+        mainSizer = wx.BoxSizer(wx.VERTICAL)
+        mainSizer.Add(self.html, 1, wx.EXPAND)
+        mainSizer.Add(self.buttonSizer, flag=wx.ALIGN_CENTER|wx.TOP, border=15)
+        self.SetSizer(mainSizer)
+
+    def SetPage(self, text):
+        self.html.SetPage(text, "")
+        return self
+
+    def GetContentWindow(self):
+        return self.html
+
+    def AddButton(self, label, buttonIdentifier):
+        if not isinstance(buttonIdentifier, int):
+            raise TypeError("buttonIdentifier must be an integer")
+        button = wx.Button(self, label=label)
+        self.buttonSizer.Add(button, 0, wx.ALL, 5)
+        self.Bind(wx.EVT_BUTTON, lambda event: self.DoButtonClick(event, buttonIdentifier), button)
+        return self
+
+    def DoButtonClick(self, event, buttonIdentifier):
+        self.EndModal(buttonIdentifier)
+
+    def OnSetFocus(self, event):
+        wx.CallAfter(self.focuser.MouseMove, self.html.GetScreenPosition())
+        wx.CallAfter(self.focuser.MouseClick) 
+        event.Skip()
+
+
 class AccessibleHTMLDialog(wx.Dialog):
     def __init__(self, parent, id, title, style=wx.DEFAULT_DIALOG_STYLE):
         wx.Dialog.__init__(self, parent, id, title, style=style)
